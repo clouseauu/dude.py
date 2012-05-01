947bd330f61c60035ab16b684d5320ffd6a9a0b0
Daniel Angel <locusdelicti@gmail.com>
2012-05-01 16:12:24 +1000

    Fixed the 'cussin' option, as it was doing the opposite of what it should have. --daniel

--- dude.py
+++ dude.py
@@ -8,7 +8,7 @@ errorstring = 'Something is wrong! They\'ve got four more developers working on 
 parser = argparse.ArgumentParser(description='Get some filler text to tie your websites together, man!')
 
 parser.add_argument('-p', '--paragraphs', dest='paragraphs', default=3, type=int, help='Number of paragraphs to generate (default: 3)')
-parser.add_argument('-c', '--no-cussin', dest='cussin', action='store_true', help='Keep it squeaky clean for client work (default: false)')
+parser.add_argument('-c', '--no-cussin', dest='cussin', action='store_false', help='Keep it squeaky clean for client work (default: false)')
 parser.add_argument('-l', '--lebowskiipsum', dest='lebowskiipsum', action='store_true', help='Start the first paragraph with "Lebowski Ipsum" (default: false)')
 parser.add_argument('-t', '--tags', dest='html', action='store_true', help='Wrap your filler text in zee HTML tacs, OK? (default: false)')
 parser.add_argument('-m', '--mixed', dest='mixed', action='store_true', help='Mix Lebowski quotes with old fashioned Lorem Ipsum (default: false)')
@@ -29,6 +29,8 @@ url = 'http://lebowskiipsum.com/dude/generate/' \
 			str(args.html).lower()
 		)
 
+
+print url
 
 try: 
 	data = urlopen(url)
